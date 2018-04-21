e9e98bfa1e1e5f8694a25fe8ca241d8b69eb95c2
Marie VerMurlen <marie@violetco.de>
2018-04-21 13:03:43 -0700

    migrations and url fixes

--- app/urls.py
+++ app/urls.py
@@ -1,18 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
-from manager.views import EntryListView
-from django.contrib.auth.decorators import login_required
-
-from manager.views import entry_search
 
 admin.autodiscover()
 admin.site.site_header = settings.SITE_NAME
 
 urlpatterns = [
-    url(r'^$', entry_search, name='home'),
     url(r'^admin42/', admin.site.urls),
     url(r'^api/', include('api.urls')),
+    url(r'^', include('coreExtend.urls')),
     url(r'^', include('manager.urls')),
-
 ]
