7a7687ed9de2cf4cb6a2fca04a89c8ff07b3cb0e
Tyler Rilling <underlost@gmail.com>
2018-04-21 14:33:55 -0700

    sidebar styles

--- app/apps/coreExtend/context_processors.py
+++ app/apps/coreExtend/context_processors.py
@@ -7,7 +7,7 @@ def template_settings(request):
         'network_desc': settings.SITE_DESC,
         'network_author': settings.SITE_AUTHOR,
         'network_url': settings.SITE_URL,
-        'network_register': settings.ALLOW_NEW_REGISTRATIONS,
+        'site_register': settings.ALLOW_NEW_REGISTRATIONS,
         'BASE_URL': 'http://' + request.get_host(),
         'BASE_HOST': request.get_host().split(':')[0],
     }
